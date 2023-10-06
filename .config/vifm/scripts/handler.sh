5f1a95ed821e8da9d11eed0a81fe25b3be14feff
shc <sh@c>
2023-10-06 12:25:57 +0200

    Centralize handler.sh

--- .config/vifm/scripts/handler.sh
+++ .config/vifm/scripts/handler.sh
@@ -17,6 +17,14 @@ function __preview() {
     cat "${@}" | format_standard ""
 }
 
+function __mpv() {
+    spawn_proc "mpv" "${@}"
+}
+
+function __info_ffmpeg() {
+    ffprobe -loglevel quiet -show_format -pretty "${1}" 2>&1 | format_standard ""
+}
+
 function main() {
     case "${1}" in
         "nvim" )
@@ -25,9 +33,14 @@ function main() {
         "preview" )
             __preview "${@:2}"
             ;;
+        "mpv" )
+            __mpv "${@:2}"
+            ;;
+        "info_ffmpeg" )
+            __info_ffmpeg "${2}"
     esac
 
-    unset -f __nvim __preview
+    unset -f __nvim __preview __mpv __info_ffmpeg
 }
 main "${@}"
 unset -f main
