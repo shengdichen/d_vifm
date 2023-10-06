9f010ba4aa68f6429a976b27bbda20dd57b19e36
shc <sh@c>
2023-10-06 03:08:46 +0200

    Move media-info read'g (ffmpeg) to media.sh

--- .config/vifm/scripts/media.sh
+++ .config/vifm/scripts/media.sh
@@ -1,6 +1,12 @@
+source "util.sh"
+
 function __mpv() {
     nohup mpv "${@}" 1>/dev/null 2>&1 &
 
+}
+
+function __info_ffmpeg() {
+    ffprobe -loglevel quiet -show_format -pretty "${1}" 2>&1 | format_standard
 }
 
 function main() {
@@ -8,9 +14,11 @@ function main() {
         "mpv" )
             __mpv "${@:2}"
             ;;
+        "info" )
+            __info_ffmpeg "${2}"
     esac
 
-    unset -f __mpv
+    unset -f __mpv __info_ffmpeg
 }
 main "${@}"
 unset -f main
