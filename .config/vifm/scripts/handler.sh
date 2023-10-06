1483907dfd32a745868360aff4e41abe74933c7c
shc <sh@c>
2023-10-06 12:38:28 +0200

    Add preview'g for dirs

--- .config/vifm/scripts/handler.sh
+++ .config/vifm/scripts/handler.sh
@@ -17,6 +17,12 @@ function __preview() {
     cat "${@}" | format_standard ""
 }
 
+function __tree() {
+    # -a := show hidden files
+    # -l := follow links
+    tree -a -l "${@}" | format_standard ""
+}
+
 function __mpv() {
     spawn_proc "mpv" "${@}"
 }
@@ -33,6 +39,9 @@ function main() {
         "preview" )
             __preview "${@:2}"
             ;;
+        "tree" )
+            __tree "${@:2}"
+            ;;
         "mpv" )
             __mpv "${@:2}"
             ;;
@@ -40,7 +49,7 @@ function main() {
             __info_ffmpeg "${2}"
     esac
 
-    unset -f __nvim __preview __mpv __info_ffmpeg
+    unset -f __nvim __preview __tree __mpv __info_ffmpeg
 }
 main "${@}"
 unset -f main
