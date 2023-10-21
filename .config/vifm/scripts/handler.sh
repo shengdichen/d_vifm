c171fd6a79b372ba405d71fadcd9db864a22997e
shc <sh@c>
2023-10-22 01:00:55 +0200

    Add batch spectrogram-handl'g

--- .config/vifm/scripts/handler.sh
+++ .config/vifm/scripts/handler.sh
@@ -245,6 +245,16 @@ function __pass() {
     fi
 }
 
+function __spectrogram() {
+    local suffix="png" outputs=()
+    for f in "${@}"; do
+        output="${f}.${suffix}"
+        sox "${f}" -n spectrogram -o "${output}"
+        outputs+=("${output}")
+    done
+    spawn_proc imv "${outputs[@]}"
+}
+
 function main() {
     case "${1}" in
         "nvim" )
@@ -262,9 +272,12 @@ function main() {
         "pass" )
             __pass "${@:2}"
             ;;
+        "spectrogram" )
+            __spectrogram "${@:2}"
+            ;;
     esac
 
-    unset -f __nvim __preview __tree __archive __pass
+    unset -f __nvim __preview __tree __archive __pass  __spectrogram
 }
 main "${@}"
 unset -f main
