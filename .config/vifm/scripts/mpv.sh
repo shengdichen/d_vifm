567a1a637ce15761f7e3febb17d0f953cb7f4814
shc <sh@c>
2024-03-13 16:58:38 +0100

    feat(mpv): add replace/append options for ipc

--- .config/vifm/scripts/mpv.sh
+++ .config/vifm/scripts/mpv.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env dash
 
 __send() {
+    local _append=""
+    while [ "${#}" -gt 0 ]; do
+        case "${1}" in
+            "--append")
+                _append="yes"
+                shift
+                ;;
+            "--")
+                shift && break
+                ;;
+            *)
+                exit 3
+                ;;
+        esac
+    done
+
     for _target in "${@}"; do
-        printf "loadfile \"%s\" append-play\n" "${_target}" | socat - ~/.local/state/mpv/throw.sok
+        if [ "${_append}" ]; then
+            printf "loadfile \"%s\" append-play\n" "${_target}"
+        else
+            printf "loadfile \"%s\" replace\n" "${_target}"
+        fi | socat - ~/.local/state/mpv/throw.sok
     done
 }
 __send "${@}"
