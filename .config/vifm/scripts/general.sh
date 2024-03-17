48b6bacc243c8e1e4b7bf1c164576b37cb9aeef4
shc <sh@c>
2024-03-17 12:12:25 +0100

    feat(general): nvim with tmp file

--- .config/vifm/scripts/general.sh
+++ .config/vifm/scripts/general.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env dash
 
+__find_files() {
+    find "${@}" -type f -print | sort
+}
+
 __fzf() {
     fzf --reverse --height=37%
+}
+
+__tmp_file() {
+    echo "${HOME}/.local/share/vifm/tmp_$(date --iso-8601=seconds)"
 }
 
 __nvim() {
@@ -13,6 +21,25 @@ __nvim() {
                     nvim -d "${@}"
                 else
                     nvim -d
+                fi
+                ;;
+            "tmp")
+                shift && shift
+                if [ "${1}" = "--" ]; then shift; fi
+                nvim \
+                    -R \
+                    -c "set nomodifiable" \
+                    -c "autocmd VimLeavePre * !rm ${1}" \
+                    -- "${1}"
+                # HACK:
+                #   avoid nvim's buggy exit-status 134, which in turns hangs vifm
+                # REF:
+                #   https://github.com/neovim/neovim/issues/21856
+                #   https://github.com/vifm/vifm/issues/527
+                if [ "${?}" -eq 134 ]; then
+                    true
+                else
+                    return
                 fi
                 ;;
             "ro")
