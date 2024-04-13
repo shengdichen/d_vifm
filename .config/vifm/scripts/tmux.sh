aeaca3210e44b4802b68305f8ed960fa23663a67
shc <sh@c>
2024-04-13 06:30:14 +0200

    chore(tmux): cleanup & reformat

--- .config/vifm/scripts/tmux.sh
+++ .config/vifm/scripts/tmux.sh
@@ -1,26 +1,12 @@
 #!/usr/bin/env dash
 
-next_pane_is_zsh() {
-    local cmd_next_pane
-    cmd_next_pane=$(tmux display-message -p -t ":.+1" -F "#{pane_current_command}")
-    [ "${cmd_next_pane}" = "zsh" ]
+__next_pane_is_zsh() {
+    local _cmd_next_pane
+    _cmd_next_pane=$(tmux display-message -p -t ":.+1" -F "#{pane_current_command}")
+    [ "${_cmd_next_pane}" = "zsh" ]
 }
 
-respawn_if_dead() {
-    if [ "$(tmux display-message -p -t "${1}" -F "#{pane_dead}")" -eq 1 ]; then
-        tmux respawn-pane
-    fi
-}
-
-cd_next_pane() {
-    # C-U := clear existing input
-    tmux send-keys -t ":.+1" \
-        "C-U" "cd ${1}" "Enter"
-
-    tmux select-pane -Z -t ":.+1"
-}
-
-create_split() {
+__create_split() {
     if [ ${#} -eq 0 ]; then
         tmux split-window -v
     elif [ ${#} -eq 1 ]; then
@@ -30,46 +16,56 @@ create_split() {
     fi
 }
 
-split_shell() {
-    if next_pane_is_zsh; then
-        cd_next_pane "${1}"
-        respawn_if_dead ":."
+__split_shell() {
+    __cd_next_pane() {
+        # C-U := clear existing input
+        tmux send-keys -t ":.+1" \
+            "C-U" "cd ${1}" "Enter"
+
+        tmux select-pane -Z -t ":.+1"
+    }
+
+    __respawn_if_dead() {
+        if [ "$(tmux display-message -p -t "${1}" -F "#{pane_dead}")" -eq 1 ]; then
+            tmux respawn-pane
+        fi
+    }
+
+    if __next_pane_is_zsh; then
+        __cd_next_pane "${1}"
+        __respawn_if_dead ":."
     else
-        create_split
+        __create_split
     fi
 }
 
-split_vifm() {
-    create_split "vifm"
-}
-
-split_file() {
-    local idx_target="1"
-    if next_pane_is_zsh; then
-        idx_target="2"
+__split_file() {
+    local _pane_id="1"
+    if __next_pane_is_zsh; then
+        _pane_id="2"
     fi
 
-    local flag
+    local _flag
     if [ "${1}" = "open" ]; then
-        flag="O"
+        _flag="O"
     elif [ "${1}" = "diff" ]; then
-        flag="d"
+        _flag="d"
     fi
 
-    create_split ":.${idx_target}" "nvim -${flag} ${2}"
+    __create_split ":.${_pane_id}" "nvim -${_flag} ${2}"
 }
 
 case "${1}" in
     "shell")
         shift
-        split_shell "${1}"
-        ;;
-    "vifm")
-        split_vifm
+        __split_shell "${1}"
         ;;
     "file")
         shift
-        split_file "${@}"
+        __split_file "${@}"
+        ;;
+    "vifm")
+        __create_split "vifm"
         ;;
     *)
         echo "Huh, what about tmux?"
