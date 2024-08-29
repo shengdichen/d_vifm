994f20f7345cf21ae1b263dc4cf856145018f41c
shc <sh@c>
2024-08-29 16:48:54 +0200

    feat(encrypt): rewrite

--- .config/vifm/scripts/encrypt.sh
+++ .config/vifm/scripts/encrypt.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env dash
 
-SCRIPT_PATH="$(realpath "$(dirname "${0}")")"
-
-. "${SCRIPT_PATH}/util.sh"
+. "${HOME}/.local/lib/util.sh"
 
 __check() {
     if [ "${1}" = "--" ]; then shift; fi
@@ -10,11 +8,76 @@ __check() {
     for _p in "${@}"; do
         case "${_p}" in
             *".md5" | \
-                *".sha1" | *".sha256" | *".sha512" | \
+                *".sha"* | \
                 *".asc" | *".sig") ;;
             *) return 1 ;;
         esac
     done
+}
+
+__calc() {
+    local _mode
+    while [ "${#}" -gt 0 ]; do
+        case "${1}" in
+            "--mode")
+                _mode="${2}"
+                shift 2
+                ;;
+            "--")
+                shift && break
+                ;;
+        esac
+    done
+
+    if [ ! "${_mode}" ]; then
+        _mode="$(
+            __fzf_opts \
+                "sha/256" "md5" "sha/512" \
+                "sha/1" "sha/224" "sha/384" "sha/512224" "sha512256"
+        )"
+    fi
+
+    case "${_mode}" in
+        "md5")
+            md5sum -- "${@}"
+            ;;
+        "sha/"*)
+            shasum \
+                --algorithm "$(printf "%s" "${_mode}" | cut -d "/" -f "2")" \
+                -- "${@}"
+            ;;
+    esac
+}
+
+__verify() {
+    local _mode
+    while [ "${#}" -gt 0 ]; do
+        case "${1}" in
+            "--mode")
+                _mode="${2}"
+                shift 2
+                ;;
+            "--")
+                shift && break
+                ;;
+        esac
+    done
+
+    if [ ! "${_mode}" ]; then
+        _mode="$(__fzf_opts "md5" "sha" "gpg")"
+    fi
+
+    case "${_mode}" in
+        "md5")
+            md5sum --check -- "${@}"
+            ;;
+        "sha")
+            shasum --check -- "${@}"
+            ;;
+        "gpg")
+            gpg --verify -- "${@}"
+            ;;
+    esac
 }
 
 __handle() {
@@ -32,46 +95,31 @@ __handle() {
     fi
     if [ "${_choice}" = "nvim" ]; then
         __nvim -- "${1}"
-    else
-        case "${1}" in
-            *".md5")
-                if [ "${_choice}" = "check" ]; then
-                    md5sum -c -- "${1}"
-                fi
-                ;;
-            *".sha1")
-                if [ "${_choice}" = "check" ]; then
-                    sha1sum -c -- "${1}"
-                fi
-                ;;
-            *".sha256")
-                if [ "${_choice}" = "check" ]; then
-                    sha256sum -c -- "${1}"
-                fi
-                ;;
-            *".sha512")
-                if [ "${_choice}" = "check" ]; then
-                    sha512sum -c -- "${1}"
-                fi
-                ;;
-            *".asc" | *".sig")
-                if [ "${_choice}" = "check" ]; then
-                    gpg --verify -- "${1}"
-                fi
-                ;;
-        esac
-        if [ "${?}" -eq 0 ]; then
-            printf "\nhash/check> success "
-            read -r _
-        else
-            printf "\nhash/check> FAIL "
-            read -r _
-        fi
+        return
     fi
+
+    if case "${1}" in
+        *".md5")
+            __verify --mode md5 -- "${1}"
+            ;;
+        *".sha"*)
+            __verify --mode sha -- "${1}"
+            ;;
+        *".asc" | *".sig")
+            __verify --mode gpg -- "${1}"
+            ;;
+    esac then
+        __separator
+        printf "hash/check> success "
+        read -r _
+    fi
+    __separator
+    printf "hash/check> FAIL "
+    read -r _
 }
 
 case "${1}" in
     *)
-        __handle "${@}"
+        __calc "${@}"
         ;;
 esac
