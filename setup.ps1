2256daa6a98b42a4b51dc99a95bc9ec0c919928a
shc/qiminfo <shengdi.chen@qiminfo.ch>
2025-05-06 13:38:57 +0200

    feat(windows/lf): do NOT overwrite marks

--- setup.ps1
+++ setup.ps1
@@ -1,12 +1,22 @@
-function CopyConfig
+function ConfigVifm
 {
     $p = "$HOME\AppData\Roaming\Vifm"
     New-Item -ItemType Directory $p -ErrorAction SilentlyContinue
 
     Copy-Item ".\common\vifm\*" $p -Recurse -Force
     Copy-Item ".\windows\vifm\*" $p -Recurse -Force
-
-    Copy-Item ".\windows\lf" "$env:LOCALAPPDATA\." -Recurse -Force
 }
 
-CopyConfig
+function ConfigLf
+{
+    Copy-Item ".\windows\lf" "$env:LOCALAPPDATA\." -Recurse -Force
+
+    $p = "$env:LOCALAPPDATA\lf\marks"
+    if (-not (Test-Path $p))
+    {
+        Copy-Item ".\windows\lf-marks" $p
+    }
+}
+
+ConfigVifm
+ConfigLf
